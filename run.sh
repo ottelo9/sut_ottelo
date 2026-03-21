8c8fbf1b0da14ae214708c1602f9e18c469a1dfa
mvgucht <michielvangucht@gmail.com>
2026-03-21 17:22:04 +0100

    Added extra environment tests in run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,51 @@
 
 set -e
 
+# Make sure that user is in dialout group.
+if ! groups | grep -q "\bdialout\b"; then
+    echo "❌ User $USER is not in the dialout group. Please add with:"
+    echo "   sudo usermod -a -G dialout $USER"
+    exit 1
+fi
+
+# Try to get the Python 3 version
+if command -v python3 &>/dev/null; then
+    PYVER=$(python3 -c 'import sys; print("{}.{}".format(sys.version_info[0], sys.version_info[1]))')
+    MIN_VER="3.8"
+
+    # Use Python for comparison
+    if ! python3 -c "import sys; sys.exit(0) if sys.version_info >= (3,8) else sys.exit(1)"; then
+        echo "❌ Python 3 version $PYVER is too old. Requires 3.8+"
+        exit 1
+    fi
+else
+    echo "❌ Python 3 is not installed"
+    exit 1
+fi
+
+DEVICE="/dev/serial0"
+
+# Check if the device exists
+if [ ! -e "$DEVICE" ]; then
+    echo "❌ $DEVICE does not exist"
+    exit 1
+fi
+
+# Check read and write permissions for the current user
+if ! ([ -r "$DEVICE" ] && [ -w "$DEVICE" ]); then
+    echo "❌ You do NOT have sufficient permissions on $DEVICE"
+
+    # Optional: show useful debug info
+    ls -l "$DEVICE"
+    id
+
+    exit 1
+fi
+
+## /dev/serial0 tests
+# socat -d -d pty,raw,echo=0 pty,raw,echo=0
+# sudo ln -s /dev/pts/3 /dev/serial0
+
 # create venv if not exists
 if [ ! -d ".venv" ]; then
     echo "Creating venv"
